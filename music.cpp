83907fae2c9b58b1245e237b724366a5a1a5d61a
slm37102 <slm37102@gmail.com>
2021-01-11 20:22:53 +0800

    comment all

--- music.cpp
+++ music.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-class Music{
-    string title, singer;
-    int duration;
-    public:
-        void AddSong(string t, string s, int d){
-            title = t;
-            singer = s;
-            duration = d;
-        }
-};
+// class Music{
+//     string title, singer;
+//     int duration;
+//     public:
+//         void AddSong(string t, string s, int d){
+//             title = t;
+//             singer = s;
+//             duration = d;
+//         }
+// };
--- music.h
+++ music.h
@@ -1,10 +1,10 @@
-#pragma once
-#include<iostream>
-using namespace std;
-struct music
-{
-    string title, singer;
-    int duration;
-    public:
-        void AddSong(string t, string s, int d);
-};
+// #pragma once
+// #include<iostream>
+// using namespace std;
+// struct music
+// {
+//     string title, singer;
+//     int duration;
+//     public:
+//         void AddSong(string t, string s, int d);
+// };
