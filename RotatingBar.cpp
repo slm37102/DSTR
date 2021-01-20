9a7f37c282ae7cb56d23aeba0cc9fe00033b5843
derick331 <75348716+derick331@users.noreply.github.com>
2021-01-20 21:16:29 +0800

    Update RotatingBar.cpp

--- RotatingBar.cpp
+++ RotatingBar.cpp
@@ -8,8 +8,8 @@ int main()
     char chars[] = { '-', '\\', '|', '/' };
     unsigned int i;
 
-    for (i = 0; ; ++i) {
-        printf("%c\r", chars[i % sizeof(chars)]);
+    for (i = 0; ; ++i) { // this loop iterates every 100ms
+        printf("%c\r", chars[i % sizeof(chars)]); // %c to print characters and \r to move the cursor back to front of this line
         fflush(stdout);
         this_thread::sleep_for(chrono::milliseconds(100));
     }
